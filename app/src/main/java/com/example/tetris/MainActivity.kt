8beb6052296e70a8bcb5d02b8e7e688803a99cee
halxvi <halxvi@gmail.com>
2020-05-27 20:50:58 +0900

    背景追加

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -4,49 +4,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val tetris = Tetris()
-        updateField(tetris.fields)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val tetris = Tetris()
+    updateField(tetris.fields)
+    updateScore(tetris.score)
+  }
 
-    private fun updateField(fields:Array<Array<Int>>){
-        fields.forEachIndexed { firstIndex, array->
-            array.forEachIndexed { lastIndex, block_num->
-                val blockArrayIndex = firstIndex * 10 + ( lastIndex + 1 )
-                val resourceId = resources.getIdentifier("block_$blockArrayIndex", "id", packageName)
-                val blockId:ImageView = findViewById(resourceId)
-                when (block_num) {
-                    1 -> {
-                        blockId.setImageResource(R.drawable.blue_block)
-                    }
-                    2 -> {
-                        blockId.setImageResource(R.drawable.red_block)
-                    }
-                    3 -> {
-                        blockId.setImageResource(R.drawable.green_block)
-                    }
-                    4 -> {
-                        blockId.setImageResource(R.drawable.orange_block)
-                    }
-                    5 -> {
-                        blockId.setImageResource(R.drawable.yellow_block)
-                    }
-                    6 -> {
-                        blockId.setImageResource(R.drawable.lightblue_block)
-                    }
-                    7 -> {
-                        blockId.setImageResource(R.drawable.purple_block)
-                    }
-                    else -> {
-                        blockId.setImageResource(R.drawable.block_background)
-                    }
-                }
-            }
+  private fun updateField(fields:Array<Array<Int>>){
+    fields.forEachIndexed { firstIndex, array->
+      array.forEachIndexed { lastIndex, block_num->
+        val blockArrayIndex = firstIndex * 10 + ( lastIndex + 1 )
+        val resourceId = resources.getIdentifier("block_$blockArrayIndex", "id", packageName)
+        val blockId:ImageView = findViewById(resourceId)
+        when (block_num) {
+          1 -> {
+            blockId.setImageResource(R.drawable.blue_block)
+          }
+          2 -> {
+            blockId.setImageResource(R.drawable.red_block)
+          }
+          3 -> {
+            blockId.setImageResource(R.drawable.green_block)
+          }
+          4 -> {
+            blockId.setImageResource(R.drawable.orange_block)
+          }
+          5 -> {
+            blockId.setImageResource(R.drawable.yellow_block)
+          }
+          6 -> {
+            blockId.setImageResource(R.drawable.lightblue_block)
+          }
+          7 -> {
+            blockId.setImageResource(R.drawable.purple_block)
+          }
+          else -> {
+            blockId.setImageResource(R.drawable.block_background)
+          }
         }
+      }
     }
+  }
+
+  private fun updateScore(score:Int){
+    scoreView.text = "Your Score:$score"
+  }
 }
--- app/src/main/java/com/example/tetris/Tetris.kt
+++ app/src/main/java/com/example/tetris/Tetris.kt
@@ -3,22 +3,22 @@ package com.example.tetris
 import kotlin.random.Random
 
 class Tetris {
-        val fields: Array<Array<Int>> = Array(20){ Array<Int>( 10){ 0 };}
-        private val straightBlock = Array(4){ 1 }
-        private val squareBlock = arrayOf(arrayOf(2,2), arrayOf(2,2))
-        private val zBlock1 = arrayOf(arrayOf(0,3,3), arrayOf(3,3,0))
-        private val zBlock2 = arrayOf(arrayOf(4,4,0), arrayOf(0,4,4))
-        private val lBlock1 = arrayOf(arrayOf(5,0,0), Array(3){ 5 })
-        private val lBlock2 = arrayOf(arrayOf(0,0,6), Array(2){ 6 })
-        private val tBlock = arrayOf(arrayOf(0,7,0), Array(3){ 7 })
-        private val blocks = arrayOf(
-            straightBlock,
-            squareBlock,
-            zBlock1,
-            zBlock2,
-            lBlock1,
-            lBlock2,
-            tBlock
-        );
-    private val score = 0;
+  val fields: Array<Array<Int>> = Array(20){ Array<Int>( 10){ 0 };}
+  val score = 0;
+  private val straightBlock = Array(4){ 1 }
+  private val squareBlock = arrayOf(arrayOf(2,2), arrayOf(2,2))
+  private val zBlock1 = arrayOf(arrayOf(0,3,3), arrayOf(3,3,0))
+  private val zBlock2 = arrayOf(arrayOf(4,4,0), arrayOf(0,4,4))
+  private val lBlock1 = arrayOf(arrayOf(5,0,0), Array(3){ 5 })
+  private val lBlock2 = arrayOf(arrayOf(0,0,6), Array(2){ 6 })
+  private val tBlock = arrayOf(arrayOf(0,7,0), Array(3){ 7 })
+  private val blocks = arrayOf(
+    straightBlock,
+    squareBlock,
+    zBlock1,
+    zBlock2,
+    lBlock1,
+    lBlock2,
+    tBlock
+  );
 }
