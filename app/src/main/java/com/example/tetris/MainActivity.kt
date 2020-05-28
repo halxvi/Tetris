9e5e6ada594f6694b9854817fc0e02cc245b785e
halxvi <halxvi@gmail.com>
2020-05-28 16:32:32 +0900

    ランダムブロック落下

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,47 +14,64 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     val tetris = Tetris()
-    updateField(tetris.fields)
-    updateScore(tetris.score)
+    val handler = Handler()
+    //var gameSpeed:Double = 1000.0
+    timer(initialDelay = 2000, period = 1000) {
+      updateField(handler, tetris.fields)
+      updateScore(handler, tetris.score)
+    }
+//    fun setTimer() {
+//      timer.cancel()
+//      timer.schedule() {
+//        updateField(tetris.fields)
+//        updateScore(tetris.score)
+//      }
+//    }
   }
 
-  private fun updateField(fields:Array<Array<Int>>){
-    fields.forEachIndexed { firstIndex, array->
-      array.forEachIndexed { lastIndex, block_num->
-        val blockArrayIndex = firstIndex * 10 + ( lastIndex + 1 )
-        val resourceId = resources.getIdentifier("block_$blockArrayIndex", "id", packageName)
-        val blockId:ImageView = findViewById(resourceId)
-        when (block_num) {
-          1 -> {
-            blockId.setImageResource(R.drawable.blue_block)
-          }
-          2 -> {
-            blockId.setImageResource(R.drawable.red_block)
-          }
-          3 -> {
-            blockId.setImageResource(R.drawable.green_block)
-          }
-          4 -> {
-            blockId.setImageResource(R.drawable.orange_block)
-          }
-          5 -> {
-            blockId.setImageResource(R.drawable.yellow_block)
-          }
-          6 -> {
-            blockId.setImageResource(R.drawable.lightblue_block)
-          }
-          7 -> {
-            blockId.setImageResource(R.drawable.purple_block)
-          }
-          else -> {
-            blockId.setImageResource(R.drawable.block_background)
+  private fun updateField(handler: Handler, fields: Array<Array<Int>>) {
+    handler.post {
+      fields.forEachIndexed { firstIndex, array ->
+        if (firstIndex > 1) {
+          array.forEachIndexed { lastIndex, block_num ->
+            val blockArrayIndex = (firstIndex - 2 ) * 10 + (lastIndex + 1)
+            val resourceId = resources.getIdentifier("block_$blockArrayIndex", "id", packageName)
+            val blockId: ImageView = findViewById(resourceId)
+            when (block_num) {
+              1 -> {
+                blockId.setImageResource(R.drawable.lightblue_block)
+              }
+              2 -> {
+                blockId.setImageResource(R.drawable.yellow_block)
+              }
+              3 -> {
+                blockId.setImageResource(R.drawable.red_block)
+              }
+              4 -> {
+                blockId.setImageResource(R.drawable.green_block)
+              }
+              5 -> {
+                blockId.setImageResource(R.drawable.blue_block)
+              }
+              6 -> {
+                blockId.setImageResource(R.drawable.orange_block)
+              }
+              7 -> {
+                blockId.setImageResource(R.drawable.purple_block)
+              }
+              else -> {
+                blockId.setImageResource(R.drawable.block_background)
+              }
+            }
           }
         }
       }
     }
   }
 
-  private fun updateScore(score:Int){
-    scoreView.text = "Your Score:$score"
+  private fun updateScore(handler: Handler, score: Int) {
+    handler.post {
+      scoreView.text = "Your Score:$score"
+    }
   }
 }
--- app/src/main/java/com/example/tetris/Tetris.kt
+++ app/src/main/java/com/example/tetris/Tetris.kt
@@ -1,24 +1,112 @@
 package com.example.tetris
 
-import kotlin.random.Random
+import java.security.SecureRandom;
+import kotlin.concurrent.timer
 
 class Tetris {
-  val fields: Array<Array<Int>> = Array(20){ Array<Int>( 10){ 0 };}
-  val score = 0;
-  private val straightBlock = Array(4){ 1 }
-  private val squareBlock = arrayOf(arrayOf(2,2), arrayOf(2,2))
-  private val zBlock1 = arrayOf(arrayOf(0,3,3), arrayOf(3,3,0))
-  private val zBlock2 = arrayOf(arrayOf(4,4,0), arrayOf(0,4,4))
-  private val lBlock1 = arrayOf(arrayOf(5,0,0), Array(3){ 5 })
-  private val lBlock2 = arrayOf(arrayOf(0,0,6), Array(2){ 6 })
-  private val tBlock = arrayOf(arrayOf(0,7,0), Array(3){ 7 })
-  private val blocks = arrayOf(
-    straightBlock,
-    squareBlock,
-    zBlock1,
-    zBlock2,
-    lBlock1,
-    lBlock2,
-    tBlock
-  );
+  private val sr: SecureRandom = SecureRandom.getInstance("SHA1PRNG")
+  val fields: Array<Array<Int>> = Array(22) { Array<Int>(10) { 0 }; }
+  var score = 0;
+  private var nextBlocks: MutableList<Int> = MutableList(10) {
+    sr.nextInt(7) + 1
+  }
+
+  private fun addBlock() {
+    val nextBlock = this.nextBlocks.first()
+    val setPlace = sr.nextInt(5) + 1
+    when (nextBlock) {
+      1 -> {
+        //straight
+        for (i in 0..3) {
+          fields[1][setPlace + i] = nextBlock
+        }
+      }
+      2 -> {
+        //square
+        for (i in 0..1) {
+          fields[0][setPlace + i] = nextBlock
+          fields[1][setPlace + i] = nextBlock
+        }
+      }
+      3 -> {
+        //z
+        for (i in 0..1) {
+          fields[0][setPlace + i + 2] = nextBlock
+          fields[1][setPlace + i] = nextBlock
+        }
+      }
+      4 -> {
+        //zReverse
+        for (i in 0..1) {
+          fields[0][setPlace + i] = nextBlock
+          fields[1][setPlace + i + 2] = nextBlock
+        }
+      }
+      5 -> {
+        //L
+        fields[0][setPlace] = nextBlock
+        for (i in 0..2) {
+          fields[1][setPlace + i] = nextBlock
+        }
+      }
+      6 -> {
+        //LReverse
+        fields[0][setPlace + 2] = nextBlock
+        for (i in 0..2) {
+          fields[1][setPlace + i] = nextBlock
+        }
+      }
+      7 -> {
+        //t
+        fields[0][setPlace + 1] = nextBlock
+        for (i in 0..2) {
+          fields[1][setPlace + i] = nextBlock
+        }
+      }
+    }
+    this.nextBlocks.removeAt(0)
+    this.nextBlocks.add(sr.nextInt(7) + 1)
+  }
+
+  private fun checkErasableBlock() {
+    for (i in 21 downTo 2) {
+      var flag: Boolean = false
+      for (n in 9 downTo 0) {
+        if (fields[i][n] == 0) {
+          flag = false
+          break
+        }
+        flag = true
+      }
+      if (flag) {
+        for (j in 9 downTo 0) {
+          fields[i][j] = 0
+        }
+        score += 1000
+      }
+    }
+  }
+
+  private fun moveBlocks() {
+    for (i in 20 downTo 0) {
+      for (n in 9 downTo 0) {
+        val currentBlock = fields[i][n]
+        val targetBlock = fields[i + 1][n]
+        if (targetBlock == 0) {
+          fields[i + 1][n] = currentBlock
+          fields[i][n] = 0
+        }
+      }
+    }
+  }
+
+  init {
+    timer(initialDelay = 2000, period = 1000) {
+      checkErasableBlock()
+      moveBlocks()
+    }
+    timer(initialDelay = 0, period = 3000) {
+      addBlock()
+    }
+  }
 }
