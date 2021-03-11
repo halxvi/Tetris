1d208a93c7e94e4bb1189b5d4a25c5e51238faee
--get-all <halxvi@gmail.com>
2021-03-11 16:48:01 +0900

    format moveblocktest

--- app/src/test/java/com/example/tetris/MoveBlockTest.kt
+++ app/src/test/java/com/example/tetris/MoveBlockTest.kt
@@ -5,19 +5,19 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class MoveBlockTest {
-    @Test
-    fun moveBlock() {
-        val tetris = Tetris()
-        //zBlock
-        tetris.nextBlocks = MutableList<Int>(3) { 4 }
-        tetris.addBlock()
-        tetris.moveBlock()
-        var expectedArray: Array<Array<Int>> = Array(23) { Array<Int>(10) { 0 } }
-        for (i in 0..9) expectedArray[22][i] = 16
-        for (i in 0..1) {
-            expectedArray[1][i] = 11
-            expectedArray[2][i + 1] = 11
-        }
-        assertArrayEquals(expectedArray, tetris.fields)
+  @Test
+  fun moveBlock() {
+    val tetris = Tetris()
+    //zBlock
+    tetris.nextBlocks = MutableList<Int>(3) { 4 }
+    tetris.addBlock()
+    tetris.moveBlock()
+    var expectedArray: Array<Array<Int>> = Array(23) { Array<Int>(10) { 0 } }
+    for (i in 0..9) expectedArray[22][i] = 16
+    for (i in 0..1) {
+      expectedArray[1][i] = 11
+      expectedArray[2][i + 1] = 11
     }
+    assertArrayEquals(expectedArray, tetris.fields)
+  }
 }
