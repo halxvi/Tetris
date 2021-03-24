2a62823f7599137cf0df87a73c0ff2107d556649
halxvi <halxvi@gmail.com>
2021-03-25 02:53:48 +0900

    ファイル構成変更

---
+++ app/src/main/java/com/example/tetris/view/GameoverFragment.kt
@@ -0,0 +1,29 @@
+package com.example.tetris.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.tetris.R
+import com.example.tetris.databinding.TetrisBinding
+import com.example.tetris.model.TetrisViewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+
+class GameoverFragment : Fragment() {
+  private val viewModel: TetrisViewModel by sharedViewModel()
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val binding: TetrisBinding =
+      DataBindingUtil.inflate(inflater, R.layout.gameover, container, false)
+    binding.lifecycleOwner = viewLifecycleOwner
+    binding.viewModel = viewModel
+    return binding.root
+  }
+}
