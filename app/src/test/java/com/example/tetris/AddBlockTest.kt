0e0cccd6fd993ba4d52c260356410d83e3f3252d
halxvi <halxvi@gmail.com>
2021-04-27 00:58:15 +0900

    test: ブロックのテスト修正

--- app/src/test/java/com/example/tetris/AddBlockTest.kt
+++ app/src/test/java/com/example/tetris/AddBlockTest.kt
@@ -1,117 +1,127 @@
 package com.example.tetris
 
-import com.example.tetris.model.field.Field
-import com.example.tetris.model.TetrisModel
-import com.example.tetris.model.TetrisModelRepository
+import com.example.tetris.block.*
+import com.example.tetris.field.Field
 import org.junit.Test
 import org.junit.Assert.*
 import org.junit.Rule
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.inject
 
 class AddBlockTest : KoinTest {
-
-  private val model: TetrisModel by inject()
-
   @get:Rule
   val koinTestRule = KoinTestRule.create {
     modules(
       module {
-        single { TetrisModelRepository() }
-        single { Field() }
-        factory { TetrisModel(get(), get()) }
-      })
+        single { (block: BlockInterface) -> Field(block) }
+      }
+    )
   }
 
   @Test
   fun addStraightBlock() {
-    model.addBlock()
-    var expectedArray: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    val field: Field by inject { parametersOf(StraightBlock()) }
+    field.addBlock()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
     for (i in 5..8) {
-      expectedArray[1][i] = 1
+      expectedBlocks[1][i] = 1
     }
-    expectedArray = addWallToArray(expectedArray)
-    assertArrayEquals(expectedArray, model.getField())
+    expectedBlocks = addWallToBlocks(expectedBlocks)
+    assertArrayEquals(expectedBlocks, field.blocks)
   }
 
-//  @Test
-//  fun addSquareBlock() {
-//    model.addSquareBlock()
-//    var expectedArray: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-//    for (i in 5..6) {
-//      expectedArray[0][i] = 2
-//      expectedArray[1][i] = 2
-//    }
-//    expectedArray = addWallToArray(expectedArray)
-//    assertArrayEquals(expectedArray, model.fields)
-//  }
-//
-//  @Test
-//  fun addZBlock() {
-//    model.addZBlock()
-//    var expectedArray: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-//    for (i in 5..6) {
-//      expectedArray[0][i] = 3
-//      expectedArray[1][i + 1] = 3
-//    }
-//    expectedArray = addWallToArray(expectedArray)
-//    assertArrayEquals(expectedArray, model.fields)
-//  }
-//
-//  @Test
-//  fun addZReverseBlock() {
-//    model.addZReverseBlock()
-//    var expectedArray: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-//    for (i in 5..6) {
-//      expectedArray[0][i + 1] = 4
-//      expectedArray[1][i] = 4
-//    }
-//    expectedArray = addWallToArray(expectedArray)
-//    assertArrayEquals(expectedArray, model.fields)
-//  }
-//
-//  @Test
-//  fun addLBlock() {
-//    model.addLBlock()
-//    var expectedArray: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-//    expectedArray[0][7] = 5
-//    for (i in 5..7) {
-//      expectedArray[1][i] = 5
-//    }
-//    expectedArray = addWallToArray(expectedArray)
-//    assertArrayEquals(expectedArray, model.fields)
-//  }
-//
-//  @Test
-//  fun addLReverseBlock() {
-//    model.addLReverseBlock()
-//    var expectedArray: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-//    expectedArray[0][5] = 6
-//    for (i in 5..7) {
-//      expectedArray[1][i] = 6
-//    }
-//    expectedArray = addWallToArray(expectedArray)
-//    assertArrayEquals(expectedArray, model.fields)
-//  }
-//
-//  @Test
-//  fun addTBlock() {
-//    model.addTBlock()
-//    var expectedArray: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-//    expectedArray[0][6] = 7
-//    for (i in 5..7) {
-//      expectedArray[1][i] = 7
-//    }
-//    expectedArray = addWallToArray(expectedArray)
-//    assertArrayEquals(expectedArray, model.fields)
-//  }
+  @Test
+  fun addSquareBlock() {
+    val field: Field by inject { parametersOf(SquareBlock()) }
+    field.addBlock()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    for (i in 5..6) {
+      expectedBlocks[0][i] = 2
+      expectedBlocks[1][i] = 2
+    }
+    expectedBlocks = addWallToBlocks(expectedBlocks)
+    assertArrayEquals(expectedBlocks, field.blocks)
+  }
 
-  private fun addWallToArray(array: Array<Array<Int>>): Array<Array<Int>> {
-    for (n in 0..21) array[n][0] = 16
-    for (n in 0..21) array[n][11] = 16
-    for (i in 2..10) array[21][i] = 16
+  @Test
+  fun addZBlock() {
+    val field: Field by inject { parametersOf(ZBlock()) }
+
+    field.addBlock()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    for (i in 5..6) {
+      expectedBlocks[0][i] = 3
+      expectedBlocks[1][i + 1] = 3
+    }
+    expectedBlocks = addWallToBlocks(expectedBlocks)
+    assertArrayEquals(expectedBlocks, field.blocks)
+  }
+
+  @Test
+  fun addZReverseBlock() {
+    val field: Field by inject { parametersOf(ZReverseBlock()) }
+
+    field.addBlock()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    for (i in 5..6) {
+      expectedBlocks[0][i + 1] = 4
+      expectedBlocks[1][i] = 4
+    }
+    expectedBlocks = addWallToBlocks(expectedBlocks)
+    assertArrayEquals(expectedBlocks, field.blocks)
+  }
+
+  @Test
+  fun addLBlock() {
+    val field: Field by inject { parametersOf(LBlock()) }
+
+    field.addBlock()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    expectedBlocks[0][7] = 5
+    for (i in 5..7) {
+      expectedBlocks[1][i] = 5
+    }
+    expectedBlocks = addWallToBlocks(expectedBlocks)
+    assertArrayEquals(expectedBlocks, field.blocks)
+  }
+
+  @Test
+  fun addLReverseBlock() {
+    val field: Field by inject { parametersOf(LReverseBlock()) }
+
+    field.addBlock()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    expectedBlocks[0][5] = 6
+    for (i in 5..7) {
+      expectedBlocks[1][i] = 6
+    }
+    expectedBlocks = addWallToBlocks(expectedBlocks)
+    assertArrayEquals(expectedBlocks, field.blocks)
+  }
+
+  @Test
+  fun addTBlock() {
+    val field: Field by inject { parametersOf(TBlock()) }
+
+    field.addBlock()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    expectedBlocks[0][6] = 7
+    for (i in 5..7) {
+      expectedBlocks[1][i] = 7
+    }
+    expectedBlocks = addWallToBlocks(expectedBlocks)
+    assertArrayEquals(expectedBlocks, field.blocks)
+  }
+
+  private fun addWallToBlocks(array: Array<Array<Int>>): Array<Array<Int>> {
+    for (n in 0..21) {
+      array[n][0] = -1
+      array[n][11] = -1
+    }
+    for (i in 2..10) array[21][i] = -1
     return array
   }
 }
--- app/src/test/java/com/example/tetris/MoveBlockTest.kt
+++ app/src/test/java/com/example/tetris/MoveBlockTest.kt
@@ -1,45 +1,46 @@
 package com.example.tetris
 
-import com.example.tetris.model.field.Field
-import com.example.tetris.model.TetrisModel
-import com.example.tetris.model.TetrisModelRepository
+import com.example.tetris.block.BlockInterface
+import com.example.tetris.block.StraightBlock
+import com.example.tetris.field.Field
 import org.junit.Test
 import org.junit.Assert.*
 import org.junit.Rule
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.inject
 
 class MoveBlockTest : KoinTest {
-
-  private val model: TetrisModel by inject()
-
   @get:Rule
   val koinTestRule = KoinTestRule.create {
     modules(
       module {
-        single { TetrisModelRepository() }
-        single { Field() }
-        factory { TetrisModel(get(), get()) }
-      })
+        single { (block: BlockInterface) -> Field(block) }
+      }
+    )
   }
 
   @Test
   fun moveBlock() {
-    model.moveBlock()
-    var expectedArray: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    val field: Field by inject { parametersOf(StraightBlock()) }
+    field.moveBlock()
+    val combinedBlocks = field.combineBlocks()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
     for (i in 5..8) {
-      expectedArray[2][i] = 1
+      expectedBlocks[2][i] = 1
     }
-    expectedArray = addWallToArray(expectedArray)
-    assertArrayEquals(expectedArray, model.getField())
+    expectedBlocks = addWallToBlocks(expectedBlocks)
+    assertArrayEquals(expectedBlocks, combinedBlocks)
   }
 
-  private fun addWallToArray(array:Array<Array<Int>>): Array<Array<Int>> {
-    for (n in 0..21) array[n][0] = 16
-    for (n in 0..21) array[n][11] = 16
-    for (i in 2..10) array[21][i] = 16
+  private fun addWallToBlocks(array: Array<Array<Int>>): Array<Array<Int>> {
+    for (n in 0..21) {
+      array[n][0] = -1
+      array[n][11] = -1
+    }
+    for (i in 2..10) array[21][i] = -1
     return array
   }
 }
