8e2988813dfa0e516f1b6ae6062d785452383ba4
halxvi <halxvi@gmail.com>
2021-04-30 01:58:41 +0900

    test: move系のテストを追加

--- app/src/test/java/com/example/tetris/MoveBlockTest.kt
+++ app/src/test/java/com/example/tetris/MoveBlockTest.kt
@@ -1,6 +1,7 @@
 package com.example.tetris
 
 import com.example.tetris.block.BlockInterface
+import com.example.tetris.block.InitBlock
 import com.example.tetris.block.StraightBlock
 import com.example.tetris.model.Field
 import org.junit.Test
@@ -11,28 +12,129 @@ import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.inject
+import kotlin.random.Random
 
 class MoveBlockTest : KoinTest {
   @get:Rule
   val koinTestRule = KoinTestRule.create {
     modules(
       module {
-        single { (block: BlockInterface) -> Field(block) }
+        single { (
+                   selectedBlocks: BlockInterface,
+                   blocks: Array<Array<Int>>,
+                   nextBlocks: MutableList<Int>,
+                   heldBlock: Int,
+                   random: Random
+                 ) ->
+          Field(
+            selectedBlocks,
+            blocks,
+            nextBlocks,
+            heldBlock,
+            random
+          )
+        }
       }
     )
   }
 
   @Test
   fun moveBlock() {
-    val field: Field by inject { parametersOf(StraightBlock()) }
-    field.moveBlock()
-    val combinedBlocks = field.combineBlocks()
-    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-    for (i in 5..8) {
-      expectedBlocks[2][i] = 1
+    val field: Field by inject {
+      parametersOf(
+        InitBlock(),
+        Array(22) { Array<Int>(12) { 0 } },
+        mutableListOf(0, 0, 0),
+        0,
+        Random
+      )
     }
-    expectedBlocks = addWallToBlocks(expectedBlocks)
-    assertArrayEquals(expectedBlocks, combinedBlocks)
+    field.addBlock()
+    field.moveBlock()
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    expectedBlocks.apply {
+      insertBlock(expectedBlocks, field.selectedBlock.type, 1)
+      addWallToBlocks(expectedBlocks)
+    }
+    assertArrayEquals(expectedBlocks, field.combineBlocks())
+  }
+
+  @Test
+  fun canMoveBlock() {
+    val field: Field by inject {
+      parametersOf(
+        InitBlock(),
+        Array(22) { Array<Int>(12) { 0 } },
+        mutableListOf(0, 0, 0),
+        0,
+        Random
+      )
+    }
+    assertTrue(field.checkMoveBlock())
+  }
+
+  @Test
+  fun cantMoveBlockWithCollapse() {
+    var blocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    blocks.apply { insertBlock(blocks, 1, 1) }
+    val field: Field by inject {
+      parametersOf(
+        StraightBlock(),
+        blocks,
+        mutableListOf(0, 0, 0),
+        0,
+        Random
+      )
+    }
+    assertFalse(field.checkMoveBlock())
+  }
+
+  @Test
+  fun cantMoveBlockWithBottom() {
+    val field: Field by inject {
+      parametersOf(
+        StraightBlock(
+          arrayOf(
+            arrayOf(5, 20),
+            arrayOf(6, 20),
+            arrayOf(7, 20),
+            arrayOf(8, 20)
+          )
+        ),
+        Array(22) { Array<Int>(12) { 0 } },
+        mutableListOf(0, 0, 0),
+        0,
+        Random
+      )
+    }
+    assertFalse(field.checkMoveBlock())
+  }
+
+  @Test
+  fun moveBlockToBottom() {
+    val field: Field by inject {
+      parametersOf(
+        InitBlock(),
+        Array(22) { Array<Int>(12) { 0 } },
+        mutableListOf(0, 0, 0),
+        0,
+        Random
+      )
+    }
+    field.addBlock()
+    while (field.checkMoveBlock()) {
+      field.moveBlock()
+    }
+    var expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    expectedBlocks.apply {
+      insertBlock(
+        expectedBlocks,
+        field.selectedBlock.type,
+        if (field.selectedBlock.type == 1) 20 else 19
+      )
+      addWallToBlocks(expectedBlocks)
+    }
+    assertArrayEquals(expectedBlocks, field.combineBlocks())
   }
 
   private fun addWallToBlocks(array: Array<Array<Int>>): Array<Array<Int>> {
@@ -42,5 +144,48 @@ class MoveBlockTest : KoinTest {
     }
     for (i in 2..10) array[21][i] = -1
     return array
+  }
+
+  private fun insertBlock(
+    expectedBlocks: Array<Array<Int>>,
+    type: Int,
+    targetY: Int
+  ): Array<Array<Int>> {
+    when (type) {
+      1 -> for (i in 5..8) {
+        expectedBlocks[targetY][i] = 1
+      }
+      2 -> for (i in 5..6) {
+        expectedBlocks[targetY][i] = 2
+        expectedBlocks[targetY + 1][i] = 2
+      }
+      3 -> for (i in 5..6) {
+        expectedBlocks[targetY][i] = 3
+        expectedBlocks[targetY + 1][i + 1] = 3
+      }
+      4 -> for (i in 5..6) {
+        expectedBlocks[targetY][i + 1] = 4
+        expectedBlocks[targetY + 1][i] = 4
+      }
+      5 -> {
+        expectedBlocks[targetY][7] = 5
+        for (i in 5..7) {
+          expectedBlocks[targetY + 1][i] = 5
+        }
+      }
+      6 -> {
+        expectedBlocks[targetY][5] = 6
+        for (i in 5..7) {
+          expectedBlocks[targetY + 1][i] = 6
+        }
+      }
+      7 -> {
+        expectedBlocks[targetY][6] = 7
+        for (i in 5..7) {
+          expectedBlocks[targetY + 1][i] = 7
+        }
+      }
+    }
+    return expectedBlocks
   }
 }
