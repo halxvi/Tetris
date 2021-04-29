f0c1a4bee8286d51d960c0ff3cc50c8fc9e94c4d
halxvi <halxvi@gmail.com>
2021-04-30 01:55:18 +0900

    chore: ストレートブロックの初期座標を修正

--- app/src/main/java/com/example/tetris/block/StraightBlock.kt
+++ app/src/main/java/com/example/tetris/block/StraightBlock.kt
@@ -2,10 +2,10 @@ package com.example.tetris.block
 
 class StraightBlock(
   override var coordinates: Array<Array<Int>> = arrayOf(
-    arrayOf(5, 1),
-    arrayOf(6, 1),
-    arrayOf(7, 1),
-    arrayOf(8, 1)
+    arrayOf(5, 0),
+    arrayOf(6, 0),
+    arrayOf(7, 0),
+    arrayOf(8, 0)
   ),
   override var direction: Int = 0,
   override val type: Int = 1
