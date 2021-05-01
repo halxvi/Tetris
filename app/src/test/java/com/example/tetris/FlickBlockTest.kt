ce6a41f64f6d14be3d2746c03d1039165804d829
halxvi <halxvi@gmail.com>
2021-05-01 18:45:05 +0900

    test: ブロックフリックテスト修正

--- app/src/test/java/com/example/tetris/FlickBlockTest.kt
+++ app/src/test/java/com/example/tetris/FlickBlockTest.kt
@@ -1,5 +1,7 @@
 package com.example.tetris
 
+import com.example.tetris.Utilities.Companion.addWallToBlocks
+import com.example.tetris.Utilities.Companion.insertBlock
 import com.example.tetris.block.BlockInterface
 import com.example.tetris.block.StraightBlock
 import com.example.tetris.model.Field
@@ -17,13 +19,7 @@ class FlickBlockTest : KoinTest {
   val koinTestRule = KoinTestRule.create {
     modules(
       module {
-        single { (
-                   selectedBlocks: BlockInterface
-                 ) ->
-          Field(
-            selectedBlocks
-          )
-        }
+        single { (selectedBlocks: BlockInterface) -> Field(selectedBlocks) }
       }
     )
   }
@@ -79,7 +75,7 @@ class FlickBlockTest : KoinTest {
     field.flickToRight()
     val expectedBlocks = Array(22) { Array<Int>(12) { 0 } }
     expectedBlocks.apply {
-      insertBlock(expectedBlocks, 1, 7, 1)
+      insertBlock(expectedBlocks, field.selectedBlock.type, 7, 1)
       addWallToBlocks(expectedBlocks)
     }
     assertArrayEquals(expectedBlocks, field.combineBlocks())
@@ -140,58 +136,5 @@ class FlickBlockTest : KoinTest {
       addWallToBlocks(expectedBlocks)
     }
     assertArrayEquals(expectedBlocks, field.combineBlocks())
-  }
-
-  private fun addWallToBlocks(array: Array<Array<Int>>): Array<Array<Int>> {
-    for (n in 0..21) {
-      array[n][0] = -1
-      array[n][11] = -1
-    }
-    for (i in 2..10) array[21][i] = -1
-    return array
-  }
-
-  private fun insertBlock(
-    expectedBlocks: Array<Array<Int>>,
-    type: Int,
-    x: Int,
-    y: Int
-  ): Array<Array<Int>> {
-    when (type) {
-      1 -> for (i in x..x + 3) {
-        expectedBlocks[y][i] = 1
-      }
-      2 -> for (i in x..x + 1) {
-        expectedBlocks[y][i] = 2
-        expectedBlocks[y + 1][i] = 2
-      }
-      3 -> for (i in x..x + 1) {
-        expectedBlocks[y][i] = 3
-        expectedBlocks[y + 1][i + 1] = 3
-      }
-      4 -> for (i in x..x + 1) {
-        expectedBlocks[y][i + 1] = 4
-        expectedBlocks[y + 1][i] = 4
-      }
-      5 -> {
-        expectedBlocks[y][x + 3] = 5
-        for (i in x..x + 3) {
-          expectedBlocks[y + 1][i] = 5
-        }
-      }
-      6 -> {
-        expectedBlocks[y][x] = 6
-        for (i in x..x + 3) {
-          expectedBlocks[y + 1][x + i] = 6
-        }
-      }
-      7 -> {
-        expectedBlocks[y][x + 1] = 7
-        for (i in x..x + 3) {
-          expectedBlocks[y + 1][x + i] = 7
-        }
-      }
-    }
-    return expectedBlocks
   }
 }
