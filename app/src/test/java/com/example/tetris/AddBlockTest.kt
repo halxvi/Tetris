b083ef038e03cdee3afb08b02ea8a3834cc97b2a
halxvi <halxvi@gmail.com>
2021-05-03 22:47:25 +0900

    test: addblockをparameterizedに対応

--- app/src/test/java/com/example/tetris/AddBlockTest.kt
+++ app/src/test/java/com/example/tetris/AddBlockTest.kt
@@ -3,114 +3,39 @@ package com.example.tetris
 import com.example.tetris.Utilities.Companion.addWallToBlocks
 import com.example.tetris.Utilities.Companion.insertBlock
 import com.example.tetris.model.Field
-import org.junit.Test
 import org.junit.Assert.*
-import org.junit.Rule
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.koin.dsl.module
 import org.koin.test.KoinTest
-import org.koin.test.KoinTestRule
 import org.koin.test.inject
+import org.koin.test.junit5.KoinTestExtension
 
 class AddBlockTest : KoinTest {
-  @get:Rule
-  val koinTestRule = KoinTestRule.create {
+  @JvmField
+  @RegisterExtension
+  val koinTestExtension = KoinTestExtension.create {
     modules(
       module {
         single {
           Field()
         }
-      }
-    )
+      })
   }
 
-  @Test
-  fun addStraightBlock() {
+  @ParameterizedTest
+  @CsvSource("1", "2", "3", "4", "5", "7")
+  fun addBlock(blockType: Int) {
     val field: Field by inject()
-    field.nextBlocks[0] = 1
+    field.nextBlocks[0] = blockType
     field.addBlock()
-    val expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
+    val expectedBlocks: Array<Array<Int>> = Array(24) { Array<Int>(12) { 0 } }
     expectedBlocks.apply {
-      insertBlock(expectedBlocks, field.selectedBlock.type, 5, 0)
+      insertBlock(expectedBlocks, field.selectedBlock.type, 5, 2)
       addWallToBlocks(expectedBlocks)
     }
-    assertArrayEquals(expectedBlocks, field.combineBlocks())
-  }
-
-  @Test
-  fun addSquareBlock() {
-    val field: Field by inject()
-    field.nextBlocks[0] = 2
-    field.addBlock()
-    val expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-    expectedBlocks.apply {
-      insertBlock(expectedBlocks, field.selectedBlock.type, 5, 0)
-      addWallToBlocks(expectedBlocks)
-    }
-    assertArrayEquals(expectedBlocks, field.combineBlocks())
-  }
-
-  @Test
-  fun addZBlock() {
-    val field: Field by inject()
-    field.nextBlocks[0] = 3
-    field.addBlock()
-    val expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-    expectedBlocks.apply {
-      insertBlock(expectedBlocks, field.selectedBlock.type, 5, 0)
-      addWallToBlocks(expectedBlocks)
-    }
-    assertArrayEquals(expectedBlocks, field.combineBlocks())
-  }
-
-  @Test
-  fun addZReverseBlock() {
-    val field: Field by inject()
-    field.nextBlocks[0] = 4
-    field.addBlock()
-    val expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-    expectedBlocks.apply {
-      insertBlock(expectedBlocks, field.selectedBlock.type, 5, 0)
-      addWallToBlocks(expectedBlocks)
-    }
-    assertArrayEquals(expectedBlocks, field.combineBlocks())
-  }
-
-  @Test
-  fun addLBlock() {
-    val field: Field by inject()
-    field.nextBlocks[0] = 5
-    field.addBlock()
-    val expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-    expectedBlocks.apply {
-      insertBlock(expectedBlocks, field.selectedBlock.type, 5, 0)
-      addWallToBlocks(expectedBlocks)
-    }
-    assertArrayEquals(expectedBlocks, field.combineBlocks())
-  }
-
-  @Test
-  fun addLReverseBlock() {
-    val field: Field by inject()
-    field.nextBlocks[0] = 6
-    field.addBlock()
-    val expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-    expectedBlocks.apply {
-      insertBlock(expectedBlocks, field.selectedBlock.type, 5, 0)
-      addWallToBlocks(expectedBlocks)
-    }
-    assertArrayEquals(expectedBlocks, field.combineBlocks())
-  }
-
-  @Test
-  fun addTBlock() {
-    val field: Field by inject()
-    field.nextBlocks[0] = 7
-    field.addBlock()
-    val expectedBlocks: Array<Array<Int>> = Array(22) { Array<Int>(12) { 0 } }
-    expectedBlocks.apply {
-      insertBlock(expectedBlocks, field.selectedBlock.type, 5, 0)
-      addWallToBlocks(expectedBlocks)
-    }
+    assertEquals(field.selectedBlock.type, blockType)
     assertArrayEquals(expectedBlocks, field.combineBlocks())
   }
 }
