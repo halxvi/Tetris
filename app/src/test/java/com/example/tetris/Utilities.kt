2d593a169eaedfa8a3bf7bba9ecb31d007a7b5d2
halxvi <halxvi@gmail.com>
2021-05-04 16:18:04 +0900

    test: xが大きいと元の配列を返すように修正

--- app/src/test/java/com/example/tetris/Utilities.kt
+++ app/src/test/java/com/example/tetris/Utilities.kt
@@ -18,34 +18,49 @@ class Utilities {
       y: Int
     ): Array<Array<Int>> {
       when (type) {
-        1 -> for (i in 0..3) {
-          expectedBlocks[y][x + i] = 1
+        1 -> {
+          if (x > 7) return expectedBlocks
+          for (i in 0..3) {
+            expectedBlocks[y][x + i] = 1
+          }
         }
-        2 -> for (i in 0..1) {
-          expectedBlocks[y][x + i] = 2
-          expectedBlocks[y + 1][x + i] = 2
+        2 -> {
+          if (x > 9) return expectedBlocks
+          for (i in 0..1) {
+            expectedBlocks[y][x + i] = 2
+            expectedBlocks[y + 1][x + i] = 2
+          }
         }
-        3 -> for (i in 0..1) {
-          expectedBlocks[y][x + i] = 3
-          expectedBlocks[y + 1][x + i + 1] = 3
+        3 -> {
+          if (x > 7) return expectedBlocks
+          for (i in 0..1) {
+            expectedBlocks[y][x + i] = 3
+            expectedBlocks[y + 1][x + i + 1] = 3
+          }
         }
-        4 -> for (i in 0..1) {
-          expectedBlocks[y][x + i + 1] = 4
-          expectedBlocks[y + 1][x + i] = 4
+        4 -> {
+          if (x > 7) return expectedBlocks
+          for (i in 0..1) {
+            expectedBlocks[y][x + i + 1] = 4
+            expectedBlocks[y + 1][x + i] = 4
+          }
         }
         5 -> {
+          if (x > 7) return expectedBlocks
           expectedBlocks[y][x + 2] = 5
           for (i in 0..2) {
             expectedBlocks[y + 1][x + i] = 5
           }
         }
         6 -> {
+          if (x > 7) return expectedBlocks
           expectedBlocks[y][x] = 6
           for (i in 0..2) {
             expectedBlocks[y + 1][x + i] = 6
           }
         }
         7 -> {
+          if (x > 8) return expectedBlocks
           expectedBlocks[y][x + 1] = 7
           for (i in 0..2) {
             expectedBlocks[y + 1][x + i] = 7
