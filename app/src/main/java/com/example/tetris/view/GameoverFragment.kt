23a98213f4ff1ce8172197a051886bd7e7f23ca1
halxvi <halxvi@gmail.com>
2021-07-11 21:18:57 +0900

    feat binding設定

--- app/src/main/java/com/example/tetris/view/GameoverFragment.kt
+++ app/src/main/java/com/example/tetris/view/GameoverFragment.kt
@@ -7,23 +7,23 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.tetris.R
-import com.example.tetris.databinding.TetrisBinding
+import com.example.tetris.databinding.GameoverBinding
 import com.example.tetris.viewmodel.TetrisViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 
 class GameoverFragment : Fragment() {
   private val viewModel: TetrisViewModel by sharedViewModel()
+  private lateinit var binding: GameoverBinding
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val binding: TetrisBinding =
-      DataBindingUtil.inflate(inflater, R.layout.gameover, container, false)
-    binding.lifecycleOwner = viewLifecycleOwner
+    binding = DataBindingUtil.inflate(inflater, R.layout.gameover, container, false)
     binding.viewModel = viewModel
+    binding.lifecycleOwner = this
     return binding.root
   }
 }
