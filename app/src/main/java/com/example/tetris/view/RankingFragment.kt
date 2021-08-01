fae73aaa66d7688af83c5a53a770cc67dd729ace
halxvi <halxvi@gmail.com>
2021-08-01 19:56:35 +0900

    feat ランキング見た目

---
+++ app/src/main/java/com/example/tetris/view/RankingFragment.kt
@@ -0,0 +1,26 @@
+package com.example.tetris.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tetris.databinding.GameoverBinding
+import com.example.tetris.viewmodel.TetrisViewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+class RankingFragment : Fragment() {
+  private val viewModel: TetrisViewModel by sharedViewModel()
+  private lateinit var binding: GameoverBinding
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    binding = GameoverBinding.inflate(inflater, container, false)
+    binding.viewModel = viewModel
+    binding.lifecycleOwner = this
+    return binding.root
+  }
+}
