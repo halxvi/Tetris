2a44cef1d65d5d63985f8e69b1c9b1fb217267aa
halxvi <halxvi@gmail.com>
2021-08-03 00:06:48 +0900

    chore レイアウト変更

--- app/src/main/java/com/example/tetris/view/AuthenticationFragment.kt
+++ app/src/main/java/com/example/tetris/view/AuthenticationFragment.kt
@@ -5,22 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.tetris.databinding.GameoverBinding
-import com.example.tetris.viewmodel.TetrisViewModel
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import com.example.tetris.R
 
 class AuthenticationFragment : Fragment() {
-  private val viewModel: TetrisViewModel by sharedViewModel()
-  private lateinit var binding: GameoverBinding
-
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View {
-    binding = GameoverBinding.inflate(inflater, container, false)
-    binding.viewModel = viewModel
-    binding.lifecycleOwner = this
-    return binding.root
+  ): View? {
+    return inflater.inflate(R.layout.authentication, container, false)
   }
 }
