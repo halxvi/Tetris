409b8b1fcc56dbf02afdd78fe4167acbaec0534a
halxvi <halxvi@gmail.com>
2021-08-03 23:30:06 +0900

    chore ランキング登録画面追加

---
+++ app/src/main/java/com/example/tetris/view/RankingSubmitFormFragment.kt
@@ -0,0 +1,18 @@
+package com.example.tetris.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tetris.R
+
+class RankingSubmitFormFragment : Fragment() {
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return inflater.inflate(R.layout.ranking_submit_form, container, false)
+  }
+}
