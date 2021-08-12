8df8a03649d36491697ece2ac891e196752f5555
halxvi <halxvi@gmail.com>
2021-08-12 22:57:30 +0900

    feat: viewbindingに変更

--- app/src/main/java/com/example/tetris/view/GameoverFragment.kt
+++ app/src/main/java/com/example/tetris/view/GameoverFragment.kt
@@ -6,22 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.tetris.databinding.GameoverBinding
-import com.example.tetris.viewmodel.TetrisViewModel
 import kotlinx.android.synthetic.main.gameover.*
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class GameoverFragment : Fragment() {
-  private val viewModel: TetrisViewModel by sharedViewModel()
-  private lateinit var binding: GameoverBinding
+  private var _binding: GameoverBinding? = null
+  private val binding get() = _binding!!
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    binding = GameoverBinding.inflate(inflater, container, false)
-    binding.viewModel = viewModel
-    binding.lifecycleOwner = this
+    _binding = GameoverBinding.inflate(inflater, container, false)
     return binding.root
   }
 
@@ -37,5 +33,10 @@ class GameoverFragment : Fragment() {
         transaction.commit()
       }
     }
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
   }
 }
--- app/src/main/java/com/example/tetris/view/RankingSubmitFormFragment.kt
+++ app/src/main/java/com/example/tetris/view/RankingSubmitFormFragment.kt
@@ -5,19 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.tetris.R
+import com.example.tetris.databinding.RankingSubmitFormBinding
 import com.example.tetris.model.Repository
 import kotlinx.android.synthetic.main.ranking_submit_form.*
 
 class RankingSubmitFormFragment : Fragment() {
   private val repository = Repository()
+  private var _binding: RankingSubmitFormBinding? = null
+  private val binding get() = _binding!!
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.ranking_submit_form, container, false)
+  ): View {
+    _binding = RankingSubmitFormBinding.inflate(inflater, container, false)
+    return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,5 +29,10 @@ class RankingSubmitFormFragment : Fragment() {
       val text = editTextTextPersonName.text.toString()
       repository.write(text)
     }
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
   }
 }
