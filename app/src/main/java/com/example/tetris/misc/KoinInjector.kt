eeee210ef36e230a1f2a3a009e39ee57c30cc766
halxvi <halxvi@gmail.com>
2021-11-07 22:09:27 +0900

    chore injector

--- app/src/main/java/com/example/tetris/misc/KoinInjector.kt
+++ app/src/main/java/com/example/tetris/misc/KoinInjector.kt
@@ -2,10 +2,13 @@ package com.example.tetris.misc
 
 import com.example.tetris.model.Tetris
 import com.example.tetris.viewmodel.TetrisViewModel
+import com.example.tetris.viewmodel.UserViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
   single { Tetris() }
+
   viewModel { TetrisViewModel(get()) }
+  viewModel { UserViewModel() }
 }
