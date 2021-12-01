28764aaae888aba7ce2b94ec7ac331f087c85dd4
halxvi <halxvi@gmail.com>
2021-12-01 23:07:05 +0900

    build koinエラー解消

--- app/src/main/java/com/example/tetris/TetrisApplication.kt
+++ app/src/main/java/com/example/tetris/TetrisApplication.kt
@@ -5,12 +5,13 @@ import com.example.tetris.misc.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class TetrisApplication : Application() {
   override fun onCreate() {
     super.onCreate()
     startKoin {
-      androidLogger()
+      androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
       androidContext(this@TetrisApplication)
       modules(appModule)
     }
